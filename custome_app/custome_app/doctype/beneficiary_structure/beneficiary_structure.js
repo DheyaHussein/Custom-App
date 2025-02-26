57fd7dabc43f06e48627c6c3f11d304e1062e5a0
DheyaHussein <dihyaa187@gmail.com>
2025-02-26 23:45:04 +0300

    feat: Add new DocTypes for Supply Party, Received Information, and Beneficiary Structure with initial configurations

---
+++ custome_app/custome_app/doctype/beneficiary_structure/beneficiary_structure.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Dheya and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Beneficiary Structure", {
+// 	refresh(frm) {
+
+// 	},
+// });
